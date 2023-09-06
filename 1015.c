a0ab8a2cb112b9ae5496946ac9d0def7c6a873fa
oclava <matheuscmabreu@gmail.com>
2023-09-06 20:42:34 -0300

    1015

--- 1015.c
+++ 1015.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+#include <math.h>
 int main () {
+    double x1,x2,y1,y2,raiz;
+    scanf("%lf%lf",&x1,&y1);
+    scanf("%lf%lf",&x2,&y2);
+
+    x2-=x1;
+    x2*=x2;
+    y2-=y1;
+    y2*=y2;
+    x2+=y2;
+    raiz = sqrt(x2);
+
+    printf("%.4lf\n", raiz);
+
+    
     return 0;
 }
