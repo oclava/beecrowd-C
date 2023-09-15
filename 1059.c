1005175464e54751def198a15b7f8bc21e70e364
Matheus Abreu <107859047+oclava@users.noreply.github.com>
2023-09-15 09:46:29 -0300

    Create 1059.c

---
+++ 1059.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int numero = 1; numero <= 100; numero++)
+    {
+        if (numero%2 == 0)
+        {
+            printf("%d\n", numero);
+        }
+        
+
+    }
+        
+    return 0;
+}
